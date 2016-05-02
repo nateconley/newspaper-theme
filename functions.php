457c1774295b6f65463ed8344c6842099455fb75
Nathan Conley <nateconley123@gmail.com>
2016-05-02 12:44:09 -0700

    Added gulp

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 function newspaper_theme_styles() {
 
 	wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
-	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/css/styles.css' );
 }
 
 add_action( 'wp_enqueue_scripts', 'newspaper_theme_styles' );
---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+	return gulp.src('./dev/styles.sass')
+		.pipe(sass())
+		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', ['sass'], function(){
+	gulp.watch('dev/style.sass', ['sass']);
+});
+
+gulp.task('default', ['watch'], function(){
+	console.log("gulp is running");
+});
